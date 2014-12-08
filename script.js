cc4baae7e68ef2b86a1f4fbb75b5d64d47013198
RodolpheAubry <gladospatate@gmail.com>
2014-12-08 09:39:49 +0100

    Delete
    Servait à rien

--- script.js
+++
@@ -1,8 +0,0 @@
-function
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
-var imageObj = new Image();
-imageObj.onload = function() {
-        context.drawImage(imageObj, 310, 420,75,75);
- };
-imageObj.src = 'img/hamster.png';
